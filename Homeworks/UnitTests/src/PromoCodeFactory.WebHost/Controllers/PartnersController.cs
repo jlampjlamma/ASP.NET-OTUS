fd7bcf6d836b7c1f1e5b61ee499ae067dfa82aa9
jlamma <jlampjlamma@gmail.com>
2026-04-22 19:43:18 +0300

    изменил порядок проверок в SetPartnerPromoCodeLimitAsync. Изменил получение лимита в CancelPartnerPromoCodeLimitAsync, чтобы получать "живой" лимит

--- Homeworks/UnitTests/src/PromoCodeFactory.WebHost/Controllers/PartnersController.cs
+++ Homeworks/UnitTests/src/PromoCodeFactory.WebHost/Controllers/PartnersController.cs
@@ -60,7 +60,7 @@ namespace PromoCodeFactory.WebHost.Controllers
                 return NotFound();
 
             //TODO: бизнес вопрос на самом деле, поэтому код править не буду.
-            //а если партнер не активен, разве мы должны выводить хоть какую-то инфу по нему? тем более лимиты
+            //а если партнер не активен, разве мы должны выводить хоть какую-то инфу по нему?
 
             var limit = partner.PartnerLimits
                 .FirstOrDefault(x => x.Id == limitId);
@@ -81,6 +81,15 @@ namespace PromoCodeFactory.WebHost.Controllers
         [HttpPost("{id}/limits")]
         public async Task<IActionResult> SetPartnerPromoCodeLimitAsync(Guid id, SetPartnerPromoCodeLimitRequest request)
         {
+            //TODO: Перенес проверку перед изменениями. Нет смысла что-то делать, если запрос не верный
+            if (request.Limit <= 0)
+                return BadRequest("Лимит должен быть больше 0");
+
+            //TODO: пропущена проверка EndDate
+            //условие не точное, т.к. не ясна бизнес логика, на сколько EndDate должен быть больше нынешнего времени
+            if (request.EndDate <= DateTime.Now)
+                return BadRequest("Дата окончания лимита ");
+
             //TODO: тут не будет Include лимитов, а значит потом мы получим null
             //самый простой в контексте добавить options.UseLazyLoadingProxies(), тем более что коллекция virtual
             //но это как по мне костыль, лучше делать отдельную реализацию репозитория для партрена с переопределением метода
@@ -94,15 +103,6 @@ namespace PromoCodeFactory.WebHost.Controllers
             //Если партнер заблокирован, то нужно выдать исключение
             if (!partner.IsActive)
                 return BadRequest("Данный партнер не активен");
-
-            //TODO: Перенес проверку перед изменениями. Нет смысла что-то делать, если запрос не верный
-            if (request.Limit <= 0)
-                return BadRequest("Лимит должен быть больше 0");
-
-            //TODO: пропущена проверка EndDate
-            //условие не точное, т.к. не ясна бизнес логика, на сколько EndDate должен быть больше нынешнего времени
-            if (request.EndDate <= DateTime.Now)
-                return BadRequest("Дата окончания лимита ");
 
             //Установка лимита партнеру
 
@@ -149,14 +149,10 @@ namespace PromoCodeFactory.WebHost.Controllers
             if (!partner.IsActive)
                 return BadRequest("Данный партнер не активен");
 
-            //TODO: опять бизнес вопрос. если EndDate меньше DateTime.Now, изменяем ли мы CancelDate? по сути он и так истек.
-            //что делаем с NumberIssuedPromoCodes? Ведь при выдаче мы его обнуляем, если EndDate еще считается активным.
-            //и если логика в итоге будет совпадать с логикой из SetPartnerPromoCodeLimitAsync,
-            //то часть с анализом и изменением CancelDate нужно вынести в отдельный метод, чтобы была единая точна
-
             //Отключение лимита
+            //TODO: Поправил логику - мы должны отменять только те, которые еще активные, т.е. EndDate > DateTime.Now
             var activeLimit = partner.PartnerLimits.FirstOrDefault(x =>
-                !x.CancelDate.HasValue);
+                !x.IsActive);
 
             if (activeLimit != null)
             {
